04edee48d889629b3f546ea09a383f3ac8d11fe0
samnelson123 <SammySalami@Sams-MBP.stny.rr.com>
2014-10-21 18:58:56 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -14,12 +14,11 @@
 -(void)update:(CCTime)delta
 {
     // put update code here
-    - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
-        // this will get called every time the player touches the screen
-        [character flap];
     }
 }
 
 // put new methods here
-
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+    // this will get called every time the player touches the screen
+    [character flap];
 @end
